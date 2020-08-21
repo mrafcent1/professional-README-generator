85af028f071901efce24fb3a283123d615a26a10
Tiffany Brand <brand2learn@gmail.com>
2020-08-21 13:23:21 -0400

    Add more comments

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const fs = require("fs");
 const util = require("util");
 const generateMd = require("./utils/generateMarkdown");
 
+// set the fs.writeFile function to use promises
 const writeFileAsync = util.promisify(fs.writeFile);
 
+// array of questions to use with inquirer 
 const questions = [
     {
         type: "input",
